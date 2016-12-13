8985693ecb4d8779f611fca616e33829faaf53be
Maruishi <maru@ihsiuram-PC.local>
2016-12-13 13:56:49 +0900

    add FTCS method

--- incomp/app/Main.hs
+++ incomp/app/Main.hs
@@ -4,7 +4,9 @@ import Lib
 import Data.Array.Repa as R
 
 type Field t = Array t DIM1 Double
-loop_n = 100 :: Int
+nx = 10 :: Int
+dx = 1.0 / (fromIntegral nx) :: Double
+dt = 2.0 / 1000 :: Double
 
 stepField :: Int -> Field U -> (Field U -> Field D) -> IO (Field U)
 stepField n u step = do
@@ -14,10 +16,31 @@ stepField n u step = do
         u' <- computeP $ step u
         stepField (n-1) u' step
 
-lst :: Field U
-lst = fromListUnboxed (Z:.100) [1.0..100.0]
+initCond0 :: Field U
+initCond0 = fromListUnboxed (Z:.nx) [1.0..(fromIntegral nx)]
+
+initCond1 :: Field U
+initCond1 = computeS $ fromFunction (Z:.nx)
+  (\(Z:.i) ->
+      let x = (fromIntegral i)*dx
+      in if (x<0.5)
+          then x
+          else (1.0-x) )
+
+-- FTCS method
+step0 :: Field U -> Field D
+step0 u = fromFunction (Z:.nx) (\(Z:.i) -> r*a(i-1) + (1.0-2.0*r)*a(i) + r*a(i+1))
+  where a j
+          |j < 0 = 0.0
+          |j >= nx = 0.0
+          |otherwise = u!(Z:.j)
+        r = dt/(dx**2)
+
+run :: Int -> IO (Field U)
+run n = do
+  u <- stepField n initCond1 step0
+  return u
 
 main :: IO ()
 main = do
-  u <- stepField 10 lst (\x -> R.map (*2) x)
-  print u
+  print "hello, world"
